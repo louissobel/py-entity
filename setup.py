fdc4a542d7ee5cc63b79b3e7297ba413fc1f1a4b
Louis Sobel <sobel@mit.edu>
2013-11-13 00:11:36 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='pyentity',
+    version='0.1.0',
+    description='Python Serializable Entities',
+    author='Louis Sobel',
+    author_email='louis.a.sobel@gmail.com',
+    url='http://github.com/louissobel/py-entity',
+    py_modules=['pyentity'],
+)
