644e51df51f1a98b71459c802cddb57575c7c68a
Louis Sobel <sobel@mit.edu>
2014-01-20 19:50:00 -0500

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
     name='pyentity',
-    version='0.1.1',
+    version='0.2.1',
     description='Python Serializable Entities',
     author='Louis Sobel',
     author_email='louis.a.sobel@gmail.com',
